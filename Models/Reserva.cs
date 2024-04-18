d3bc9214d4c2d7e9930c26640eed430575b6a5d3
Cleyton Jesus <cleytonjesus007@gmail.com>
2024-04-18 17:03:38 -0300

    finalizando o desafio DIO

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            bool ExcedeuACapacidade = Suite.Capacidade >= hospedes.Count;
+            if (ExcedeuACapacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +26,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception($"A suite em questão só permite ({Suite.Capacidade}) {(Suite.Capacidade > 1 ? "hóspedes": "hóspede")} , impossibilitando o cadastro de ({hospedes.Count}) {(hospedes.Count > 1 ? "hóspedes" : "hóspede")}.");
             }
         }
 
@@ -37,7 +39,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int quantidadeDeHospedes = Hospedes.Count;
+            return quantidadeDeHospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +48,14 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                decimal valorDesconto = (10 / 100M) * valor;
+                valor = valor - valorDesconto;
             }
 
             return valor;
